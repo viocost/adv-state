7fb96fab9f4b3a74a4d5b0ce64dc48d06896ddca
Konstantin Rybakov <viocost@gmail.com>
2021-09-04 18:37:52 +0500

    Removed unused functions from state machine

--- src/AdvStateMachine.ts
+++ src/AdvStateMachine.ts
@@ -18,6 +18,7 @@ import { State } from "./State";
 import { LoggerContainer } from "./LoggerContainer";
 import { LogFilter } from "./LogFilter";
 import { EventMapper } from "./EventMapper";
+import { StateTreeValidator } from "./StateTreeValidator";
 
 /**
  *
@@ -67,7 +68,7 @@ export class StateMachine implements IStateMachine, Visitable {
     this.error = false;
     this.name = name || "State Machine";
     this.root = this.initStateTree(stateMap);
-    this.validateStateMap(this.root);
+    this.validateStateTree(this.root);
     this.eventMap = this.mapEvents();
   }
 
@@ -109,10 +110,6 @@ export class StateMachine implements IStateMachine, Visitable {
     this.handle[messageName](payload);
   }
 
-  getOnCrashAction() {
-    return this.onCrash;
-  }
-
   mapEvents() {
     const eventMapper = new EventMapper();
 
@@ -126,214 +123,8 @@ export class StateMachine implements IStateMachine, Visitable {
     return eventMapper.getMap();
   }
 
-  validateStateMap(stateMap) {
-    /*
-        * State map constraints
-        *
-        * 1. Any state can be either leaf state or region
-        * 2. Region state must have property region: true
-        * 3. There must be exactly ONE region or state that is root.
-        * 4. Any region can be either concurrent or non-concurrent
-        * 5. Non-concurrent region has exactly one active child state when active
-        * 6. Concurrent region assumes to have one or more child regions
-        * 7. In concurrent region when active all its children regions are active
-        * 8. Concurrent region cannot have leaf states as children
-        * 9. Non-concurrent region can have memory enabled, so when re-entered, the state will be set to where it left off.
-        * 10. By default a region considered to be non-concurrent. To make region concurrent need to set concurrent: true in state map.
-                //Verify there is state map
-        */
-    if (stateMap === undefined) throw new err.noStateMap();
-
-    //Verify there is initial state
-    let initialState = [];
-    for (let state in stateMap) {
-      if (stateMap[state].initial) initialState.push(state);
-
-      //events must be at least an empty object
-      if (!stateMap[state].hasOwnProperty("events")) {
-        stateMap[state].events = {};
-      }
-    }
-
-    //Verify state map
-    if (initialState.length === 0)
-      throw new err.initStateNotInMap(
-        `Initial state provided: ${initialState} || States: ${inspect(
-          Object.keys(stateMap)
-        )}`
-      );
-    if (initialState.length > 1)
-      throw new err.multipleInitialStates(inspect(initialState));
-  }
-
-  getInitialState(stateMap) {
-    for (let state in stateMap) {
-      if (this.stateMap[state].initial) return state;
-    }
+  validateStateTree(root: State) {
+    const stateTreeValidator = new StateTreeValidator();
+    root.accept(stateTreeValidator);
   }
 }
-
-module.exports = {
-  StateMachine: StateMachine,
-  SMTraceLevel: SMTraceLevel,
-};
-
-/*
- * State map constraints
- *
- * 1. Any state can be either leaf state or region
- * 2. Region state must have property region: true
- * 3. There must be exactly ONE region or state that is root.
- * 4. Any region can be either concurrent or non-concurrent
- * 5. Non-concurrent region has exactly one active child state when active
- * 6. Concurrent region assumes to have one or more child regions
- * 7. In concurrent region when active all its children regions are active
- * 8. Concurrent region cannot have leaf states as children
- * 9. Non-concurrent region can have memory enabled, so when re-entered, the state will be set to where it left off.
- * 10. By default a region considered to be non-concurrent. To make region concurrent need to set concurrent: true in state map.
- *
- *
- * Example:
- *
- * Imagine an electircal device that has 2 modes:
- *  1. Lumen
- *  2. Music + rotation
- *
- *  It cannot do Lumen and play music/rotate at the same time.
- *
- *  Lumen mode has following substates:
- *    - green
- *    - yellow
- *    - red
- *    - off (initial)
- *
- * Music + rotation has 2 concurrent regions: music and rotation.
- *
- * Music substates are:
- *   - Playing
- *   - Off
- *
- * Rotation substates are
- *  - Off
- *  - Rotating left
- *  - Rotating right
- *
- * We also have power state, which can be on or off.
- * If power goes off it forces the system out of all states.
- *
- * Here's the state map for such machine:
- *
-  {
-     powerOff: {
-        root: true,
-        events: {
-            powerOn: {
-                state: "powerOn",
-                actions: ...
-            }
-        }
-     },
-
-     powerOn: {
-         region: true,
-         events: {
-             powerOff: {
-                 state: "powerOff",
-                 actions: ...
-             }
-         }
-     },
-
-     lumen: {
-         region: true,
-         parent: "powerOn",
-         transition: {
-             switchToMusicRotation: {
-                 state: "musicRotation"
-             }
-         }
-     },
-
-     musicRotation: {
-         region: true,
-         concurrent: true,
-         parent: "powerOn",
-         initial: true,
-         events: {
-             switchToLumen: {
-                 state: "lumen"
-             }
-         }
-
-     },
-
-     music: {
-         region: true,
-         parent: "musicRotation"
-     },
-
-     rotation: {
-         region: true,
-         parent: "musicRotation"
-     },
-
-     musicPlaying: {
-         parent: "music",
-         events: {
-             stopMusic: {
-                 state: "musicOff",
-             }
-         }
-     },
-
-     musicOff: {
-         parent: "music",
-         initial: true,
-         events: {
-             playMusic: {
-                 state: "musicPlaying",
-             }
-         }
-     },
-
-     rotationOff: {
-         parent: "rotation",
-         initial: true,
-         events: {
-             rotateLeft: {
-                 state: "rotationLeft"
-             },
-             rotateRight: {
-                 state: "rotationRight"
-             }
-         }
-     },
-
-     rotationLeft: {
-
-         parent: "rotation",
-         events: {
-             stopRotation: {
-                 state: "rotationOff"
-             },
-             rotateRight: {
-                 state: "rotationRight"
-             }
-         }
-     }
-
-     rotationRight: {
-         parent: "rotation",
-         events: {
-             rotateLeft: {
-                 state: "rotationLeft"
-             },
-             stopRotation: {
-                 state: "rotationOff"
-             }
-         }
-     }
-  }
- /
-
-*/

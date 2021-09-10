9d68bef17e9813f8a585ae3186f72da658e86c90
Konstantin Rybakov <viocost@gmail.com>
2021-09-10 22:40:02 +0500

    Removed console.logs from test bus

--- src/test/TestBus.ts
+++ src/test/TestBus.ts
@@ -7,12 +7,10 @@ export class FakeMBus extends EventEmitter {
   receivedPayloads = [];
   sm: StateMachine;
   subscribe(sm: StateMachine) {
-    console.log("Subscribe called");
     this.sm = sm;
   }
 
   deliver(msg: SMMessageBusMessage, ...rest: any) {
-    console.log("DELIVER CALLED");
     const [mName, payload] = msg;
     this.emit(mName as string, payload);
     this.receivedMessages.push(mName);
